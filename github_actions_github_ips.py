37c64b83a7d6139afb5bd91206b70a893fb38f42
GForceIndustries <52680056+GForceIndustries@users.noreply.github.com>
2025-05-07 20:38:03 +0100

    Update github_actions_github_ips.py

--- github_actions_github_ips.py
+++ github_actions_github_ips.py
@@ -27,10 +27,16 @@ def generateRefresherRsc(lists):
     writer.write('\nfile remove [find name~"^github.*ipv..rsc"]')
 
     writer.write('\n/system script')
+
+    #/system/script> remove [find name="github-ips-refresher"]
+
+    writer.write('\nremove [find name="github-ips-refresher"]')
+
+
     writer.write('\nadd dont-require-permissions=yes name=github-ips-refresher owner=admin policy=ftp,read,write,test source=":log info \\"Download GitHub IP lists\\";\\r\\')
     for list in lists:
-        writer.write('    \n\\n/tool fetch url=\\"https://raw.githubusercontent.com/GForceIndustries/mikrotik-github-ips/refs/heads/main/' + list + '.rsc\\" mode=https dst-path=' + list + '.rsc;\\r\\')
-    writer.write('    \n\\n\\r\\')
+        writer.write('\n    \\n/tool fetch url=\\"https://raw.githubusercontent.com/GForceIndustries/mikrotik-github-ips/refs/heads/main/' + list + '.rsc\\" mode=https dst-path=' + list + '.rsc;\\r\\')
+    writer.write('\n    \\n\\r\\')
 
     writer.write('\"')
     writer.close()
